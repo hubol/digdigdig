5fa66b7572439790e36bb87119eb4bbe374de82f
Hubol <hubol.gordon@gmail.com>
2025-04-05 06:56:28 -0500

    book logic tweak

--- src/igua/objects/obj-book.ts
+++ src/igua/objects/obj-book.ts
@@ -1,5 +1,6 @@
 import { Sprite } from "pixi.js";
 import { Tx } from "../../assets/textures";
+import { nlerp } from "../../lib/math/number";
 import { Rng } from "../../lib/math/rng";
 import { layers } from "../globals";
 import { mxnStaticAffectedByHoles } from "./obj-ground-stage";
@@ -7,25 +8,22 @@ import { playerObj } from "./obj-player";
 
 export function objBook(message: string) {
     let corruptedMessage = message;
-    const coveragePerCorruptedLetter = 1 / (message.length - 2);
 
     return Sprite.from(Tx.Hints.Book).anchored(0.5, 0.5)
         .mixin(mxnStaticAffectedByHoles)
-        .coro(function* (self) {
-            while (true) {
-                const coverage = self.mxnStaticAffectedByHoles.state.coverageUnit;
-                yield () => self.mxnStaticAffectedByHoles.state.coverageUnit - coverage >= coveragePerCorruptedLetter;
-                const count = (self.mxnStaticAffectedByHoles.state.coverageUnit - coverage)
-                    / coveragePerCorruptedLetter;
-                for (let i = 0; i < count; i++) {
-                    const index = Rng.int(corruptedMessage.length);
-                    const before = corruptedMessage.substring(0, index);
-                    const after = corruptedMessage.substring(index + 1);
-                    corruptedMessage = before + after;
-                }
-            }
-        })
         .step(self => {
+            const desiredLength = Math.max(
+                0,
+                Math.round(
+                    nlerp(message.length, 0, self.mxnStaticAffectedByHoles.state.coverageUnit * 1.1),
+                ),
+            );
+            while (corruptedMessage.length > desiredLength) {
+                const index = Rng.int(corruptedMessage.length);
+                const before = corruptedMessage.substring(0, index);
+                const after = corruptedMessage.substring(index + 1);
+                corruptedMessage = before + after;
+            }
             if (self.collides(playerObj) && corruptedMessage.length > 0) {
                 layers.overlay.objects.readingBookObj.methods.show(corruptedMessage);
             }
