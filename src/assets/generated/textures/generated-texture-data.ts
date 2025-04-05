bf594ebd07bbe24413324b9ca45055f1ff1f48d6
Hubol <hubol.gordon@gmail.com>
2025-04-05 03:50:08 -0500

    Decorations

--- src/assets/generated/textures/generated-texture-data.ts
+++ src/assets/generated/textures/generated-texture-data.ts
@@ -1,6 +1,6 @@
 // This file is generated
 
-const atlases = [{ url: require("./atlas0.png"), texturesCount: 30 }];
+const atlases = [{ url: require("./atlas0.png"), texturesCount: 32 }];
 
 interface TxData {
   id: string;
@@ -13,7 +13,7 @@ interface TxData {
 
 function txs<T>(tx: (data: TxData) => T) {
   return {
-    CharacterShadowSmall: tx({ id: "CharacterShadowSmall", atlas: 0, x: 0, y: 461, width: 47, height: 13 }),
+    CharacterShadowSmall: tx({ id: "CharacterShadowSmall", atlas: 0, x: 50, y: 461, width: 47, height: 13 }),
     CharacterShadow: tx({ id: "CharacterShadow", atlas: 0, x: 766, y: 277, width: 80, height: 19 }),
     Character: tx({ id: "Character", atlas: 0, x: 0, y: 0, width: 1000, height: 150 }),
     Choose: {
@@ -23,15 +23,15 @@ function txs<T>(tx: (data: TxData) => T) {
       Title: tx({ id: "Choose.Title", atlas: 0, x: 317, y: 151, width: 280, height: 67 }),
     },
     Dig: {
-      Hole: tx({ id: "Dig.Hole", atlas: 0, x: 101, y: 450, width: 49, height: 47 }),
+      Hole: tx({ id: "Dig.Hole", atlas: 0, x: 0, y: 461, width: 49, height: 47 }),
     },
     Foliage: {
       Bush0: tx({ id: "Foliage.Bush0", atlas: 0, x: 598, y: 151, width: 254, height: 125 }),
       Flower0: tx({ id: "Foliage.Flower0", atlas: 0, x: 981, y: 221, width: 36, height: 24 }),
       Grass0: tx({ id: "Foliage.Grass0", atlas: 0, x: 0, y: 417, width: 100, height: 43 }),
-      Grass1: tx({ id: "Foliage.Grass1", atlas: 0, x: 48, y: 461, width: 47, height: 25 }),
+      Grass1: tx({ id: "Foliage.Grass1", atlas: 0, x: 50, y: 475, width: 47, height: 25 }),
       Grass2: tx({ id: "Foliage.Grass2", atlas: 0, x: 0, y: 402, width: 101, height: 14 }),
-      Leaves0: tx({ id: "Foliage.Leaves0", atlas: 0, x: 102, y: 402, width: 51, height: 47 }),
+      Leaves0: tx({ id: "Foliage.Leaves0", atlas: 0, x: 101, y: 430, width: 51, height: 47 }),
       PineTree0: tx({ id: "Foliage.PineTree0", atlas: 0, x: 178, y: 333, width: 65, height: 124 }),
       Trunk0: tx({ id: "Foliage.Trunk0", atlas: 0, x: 244, y: 333, width: 11, height: 84 }),
     },
@@ -42,13 +42,17 @@ function txs<T>(tx: (data: TxData) => T) {
       Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 853, y: 256, width: 102, height: 24 }),
       GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 0, y: 204, width: 256, height: 128 }),
     },
+    Hints: {
+      Arrow: tx({ id: "Hints.Arrow", atlas: 0, x: 153, y: 430, width: 24, height: 17 }),
+      Move: tx({ id: "Hints.Move", atlas: 0, x: 102, y: 402, width: 75, height: 27 }),
+    },
     Patch: {
       Shadow0: tx({ id: "Patch.Shadow0", atlas: 0, x: 853, y: 221, width: 127, height: 34 }),
       Splotch0: tx({ id: "Patch.Splotch0", atlas: 0, x: 257, y: 219, width: 243, height: 217 }),
     },
     Shapes: {
       LightSmall0: tx({ id: "Shapes.LightSmall0", atlas: 0, x: 981, y: 246, width: 19, height: 7 }),
-      Square32: tx({ id: "Shapes.Square32", atlas: 0, x: 0, y: 475, width: 32, height: 32 }),
+      Square32: tx({ id: "Shapes.Square32", atlas: 0, x: 847, y: 281, width: 32, height: 32 }),
       VSmall0: tx({ id: "Shapes.VSmall0", atlas: 0, x: 1001, y: 0, width: 19, height: 13 }),
     },
     Stone: {
--- src/igua/scenes/scn-choose-character.ts
+++ src/igua/scenes/scn-choose-character.ts
@@ -1,9 +1,11 @@
+import { Graphics } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
 import { EscapeTickerAndExecute } from "../../lib/game-engine/asshat-ticker";
+import { interpv, interpvr } from "../../lib/game-engine/routines/interp";
 import { sleep, sleepf } from "../../lib/game-engine/routines/sleep";
 import { container } from "../../lib/pixi/container";
 import { renderer } from "../current-pixi-renderer";
-import { Key, sceneStack } from "../globals";
+import { Key, layers, scene, sceneStack } from "../globals";
 import { mxnBoilPivot } from "../mixins/mxn-boil-pivot";
 import { objCharacter, ObjCharacterArgs } from "../objects/obj-character";
 import { generateObjCharacterArgs } from "../objects/obj-npc";
@@ -32,16 +34,21 @@ function objCharacterChooser() {
             while (true) {
                 const playerObj = objChooseCharacterPlayer(generateObjCharacterArgs()).at(self).show();
                 yield sleepf(1);
-                yield () => Key.justWentDown("Space");
+                yield () => !playerObj.state.chosen && Key.justWentDown("Space");
                 playerObj.destroy();
             }
         });
 }
 
 function objChooseCharacterPlayer(args: ObjCharacterArgs) {
+    const state = {
+        chosen: false,
+    };
+
     return objCharacter(args)
+        .merge({ state })
         .step(self => {
-            if (Key.isDown("ArrowRight")) {
+            if (state.chosen || Key.isDown("ArrowRight")) {
                 self.x += 2;
                 self.controls.pedometer += 0.1;
                 self.controls.facingDirection = "east";
@@ -50,9 +57,17 @@ function objChooseCharacterPlayer(args: ObjCharacterArgs) {
                 self.controls.pedometer = 0;
             }
 
-            if (self.x >= renderer.width) {
+            if (!state.chosen && self.x >= renderer.width) {
                 setPlayerCharacterArgs(args);
-                throw new EscapeTickerAndExecute(() => sceneStack.replace(scnWorld, { useGameplay: true }));
+                state.chosen = true;
+                layers.overlay.coro(function* (self) {
+                    const gfx = new Graphics().beginFill(0).drawRect(0, 0, 700, 900).show(self).at(0, -989);
+                    gfx.angle = 45;
+                    yield interpvr(gfx).to(350, -350).over(1000);
+                    sceneStack.replace(scnWorld, { useGameplay: true });
+                    yield interpvr(gfx).to(989, 0).over(1000);
+                    gfx.destroy();
+                });
             }
         });
 }
